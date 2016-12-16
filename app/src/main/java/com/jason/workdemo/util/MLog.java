c28f0a27c3ad507c353153c84e4750f5ae9f9c45
jason0539 <jason0539@163.com>
2016-12-16 19:53:08 +0800

    log:增加e和标签

--- app/src/main/java/com/jason/workdemo/util/MLog.java
+++ app/src/main/java/com/jason/workdemo/util/MLog.java
@@ -1,21 +1,29 @@
 package com.jason.workdemo.util;
 
-import com.jason.workdemo.BuildConfig;
+import android.util.Log;
 
 /**
  * liuzhenhui 16/6/4.上午10:07
  */
 public class MLog {
-    private static final boolean DEBUG = BuildConfig.DEBUG;
-    public static final String TAG = MLog.class.getSimpleName();
+    public static final String TAG = "lzh";
+    private static boolean DEBUG = true;
 
     public static final String TAG_FILE = "TAG_FILE";
     public static final String TAG_JSON = "TAG_JSON";
     public static final String TAG_SPAN = "TAG_SPAN";
+    public static final String TAG_SCROLL = "TAG_SCROLL";
 
-    public static void d(String TAG, String msg) {
+    public static final void d(String subTag, String msg) {
         if (DEBUG) {
-            android.util.Log.d(TAG, msg);
+            Log.d(TAG, subTag + "-->" + msg);
         }
     }
+
+    public static final void e(String subTag, String msg) {
+        if (DEBUG) {
+            Log.e(TAG, subTag + "-->" + msg);
+        }
+    }
+
 }
