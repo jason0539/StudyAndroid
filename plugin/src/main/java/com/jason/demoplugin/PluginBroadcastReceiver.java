0a52d3f4716a0bfe53202c2905118ecc7c93df72
jason0539 <jason0539@163.com>
2017-02-13 16:04:38 +0800

    refactor:广播类重命名

--- plugin/src/main/java/com/jason/demoplugin/PluginBroadcastReceiver.java
+++ plugin/src/main/java/com/jason/demoplugin/PluginBroadcastReceiver.java
@@ -10,10 +10,10 @@ import android.widget.Toast;
  * Created by liuzhenhui on 2017/2/7.
  */
 
-public class BroadcastReceiverInPlugin extends BroadcastReceiver {
+public class PluginBroadcastReceiver extends BroadcastReceiver {
     @Override
     public void onReceive(Context context, Intent intent) {
-        Log.d("TAG_HOOK", "BroadcastReceiverInPlugin -> onReceive : 我是插件PluginBroadcastReceiver，收到广播");
+        Log.d("TAG_HOOK", "PluginBroadcastReceiver -> onReceive : 我是插件PluginBroadcastReceiver，收到广播");
         Toast.makeText(context, "我是插件PluginBroadcastReceiver，收到广播", Toast.LENGTH_SHORT).show();
     }
 }
