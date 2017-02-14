301500997c904901c045662fe9db2ffb9e552b0d
jason0539 <jason0539@163.com>
2017-02-14 14:55:32 +0800

    feature:插件apk中增加ContentProvider

--- plugin/src/main/java/com/jason/demoplugin/MainActivity.java
+++ plugin/src/main/java/com/jason/demoplugin/MainActivity.java
@@ -2,10 +2,14 @@ package com.jason.demoplugin;
 
 import android.app.Activity;
 import android.content.ComponentName;
+import android.content.ContentValues;
 import android.content.Intent;
+import android.database.Cursor;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
 
 //继承AppCompat的方式导致classnotfound
 //public class MainActivity extends AppCompatActivity {
@@ -17,6 +21,8 @@ public class MainActivity extends Activity {
     Button tvService2Start;
     Button tvService1End;
     Button tvService2End;
+    Button tvProviderInsert;
+    Button tvProviderQuery;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -62,7 +68,7 @@ public class MainActivity extends Activity {
             }
         });
 
-        tvService1End = (Button)findViewById(R.id.tv_service1_stop);
+        tvService1End = (Button) findViewById(R.id.tv_service1_stop);
         tvService1End.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -71,12 +77,41 @@ public class MainActivity extends Activity {
             }
         });
 
-        tvService2End = (Button)findViewById(R.id.tv_service2_stop);
+        tvService2End = (Button) findViewById(R.id.tv_service2_stop);
         tvService2End.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 stopService(new Intent().setComponent(
                         new ComponentName("com.jason.demoplugin", "com.jason.demoplugin.PluginService2")));
+            }
+        });
+
+        tvProviderInsert = (Button) findViewById(R.id.tv_provider_insert);
+        tvProviderInsert.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                ContentValues values = new ContentValues();
+                values.put(PluginContentProvider.PluginTable.PLUGIN_NAME, "test plugin mainActivity " + System.currentTimeMillis());
+                getContentResolver().insert(PluginContentProvider.PLUGIN_CONTENT_URI, values);
+            }
+        });
+
+        tvProviderQuery = (Button) findViewById(R.id.tv_provider_query);
+        tvProviderQuery.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Cursor cursor = getContentResolver().query(PluginContentProvider.PLUGIN_CONTENT_URI, null, null, null, null);
+                if (cursor != null) {
+                    if (cursor.moveToFirst()) {
+                        int index = cursor.getColumnIndex(PluginContentProvider.PluginTable.PLUGIN_NAME);
+                        if (index != -1) {
+                            String pluginName = cursor.getString(index);
+                            Log.d("TAG_HOOK", "PluginContentProvider -> query : pluginName = " + pluginName);
+                            Toast.makeText(MainActivity.this, "ContentProvider 查询 pluginName = " + pluginName, Toast.LENGTH_SHORT);
+                        }
+                    }
+                    cursor.close();
+                }
             }
         });
     }
---
+++ plugin/src/main/java/com/jason/demoplugin/PluginContentProvider.java
@@ -0,0 +1,244 @@
+package com.jason.demoplugin;
+
+import android.content.ContentProvider;
+import android.content.ContentUris;
+import android.content.ContentValues;
+import android.content.Context;
+import android.content.UriMatcher;
+import android.content.res.Resources;
+import android.database.Cursor;
+import android.database.SQLException;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+import android.net.Uri;
+import android.provider.BaseColumns;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.text.TextUtils;
+import android.util.Log;
+
+/**
+ * Created by liuzhenhui on 2017/2/13.
+ */
+
+public class PluginContentProvider extends ContentProvider {
+
+    private DatabaseHelper mDatabaseHelper;
+    private static final UriMatcher mUriMatcher;
+    private static final String[] sTestPluginProjection;
+
+    public static final String AUTHORITY = "com.jason.demoplugin.provider";
+    public static final String PATH = "plugin";
+    public static final String PATH_NUMBER = "plugin/#";
+    public static final Uri PLUGIN_CONTENT_URI = Uri.parse("content://" + AUTHORITY + "/" + PATH);
+
+    private static final int TEST_PLUGIN = 101;
+    private static final int TEST_PLUGIN_ID = 102;
+
+    static {
+        mUriMatcher = new UriMatcher(UriMatcher.NO_MATCH);
+        //匹配AUTHORITY/PATH
+        mUriMatcher.addURI(AUTHORITY, PATH, TEST_PLUGIN);
+        //匹配AUTHORITY/PATH_NUMBER
+        mUriMatcher.addURI(AUTHORITY, PATH_NUMBER, TEST_PLUGIN_ID);
+        sTestPluginProjection = new String[]{
+                PluginTable._ID, PluginTable.PLUGIN_NAME, PluginTable.CREATE_DATE, PluginTable.MODIFIED_DATE
+        };
+    }
+
+    @Override
+    public boolean onCreate() {
+        mDatabaseHelper = new DatabaseHelper(getContext());
+        return true;
+    }
+
+    @Nullable
+    @Override
+    public String getType(@NonNull Uri uri) {
+        return null;
+    }
+
+    @Nullable
+    @Override
+    public Uri insert(@NonNull Uri uri, @Nullable ContentValues values) {
+        Log.d("TAG_HOOK", "PluginContentProvider -> insert : uri = " + uri);
+        String tableName;
+        String nullColumnHack;
+        Resources resources = Resources.getSystem();
+        if (values == null) {
+            values = new ContentValues();
+        }
+
+        Long now = Long.valueOf(System.currentTimeMillis());
+
+        switch (mUriMatcher.match(uri)) {
+            case TEST_PLUGIN:
+                tableName = PluginTable.TABLE_NAME;
+                nullColumnHack = PluginTable.PLUGIN_NAME;
+                if (!values.containsKey(PluginTable.PLUGIN_NAME)) {
+                    values.put(PluginTable.PLUGIN_NAME, resources.getString(android.R.string.untitled));
+                }
+                if (!values.containsKey(PluginTable.CREATE_DATE)) {
+                    values.put(PluginTable.CREATE_DATE, now);
+                }
+                if (!values.containsKey(PluginTable.MODIFIED_DATE)) {
+                    values.put(PluginTable.MODIFIED_DATE, now);
+                }
+                break;
+            default:
+                throw new IllegalArgumentException("Unknow URI " + uri);
+        }
+        SQLiteDatabase db = mDatabaseHelper.getWritableDatabase();
+        long rowId = db.insert(tableName, nullColumnHack, values);
+        if (rowId > 0) {
+            Log.d("TAG_HOOK", "PluginContentProvider -> insert 成功 " + rowId);
+            Uri returnUri = ContentUris.withAppendedId(uri, rowId);
+            getContext().getContentResolver().notifyChange(returnUri, null);
+            return returnUri;
+        }else {
+            Log.d("TAG_HOOK", "PluginContentProvider -> insert 失败 " + rowId);
+        }
+        throw new SQLException("Failed to insert row into " + uri);
+    }
+
+    @Override
+    public int delete(@NonNull Uri uri, @Nullable String selection, @Nullable String[] selectionArgs) {
+        SQLiteDatabase db = mDatabaseHelper.getWritableDatabase();
+        String tableName;
+
+        int count;
+
+        switch (mUriMatcher.match(uri)) {
+            case TEST_PLUGIN:
+                tableName = PluginTable.TABLE_NAME;
+                break;
+            case TEST_PLUGIN_ID:
+                tableName = PluginTable.TABLE_NAME;
+                selection = PluginTable._ID + "=" + uri.getPathSegments().get(1)
+                        + (!TextUtils.isEmpty(selection) ? " AND (" + selection + ')' : "");
+                break;
+            default:
+                throw new IllegalArgumentException("Unknown URI " + uri);
+        }
+
+        count = db.delete(tableName, selection, selectionArgs);
+        getContext().getContentResolver().notifyChange(uri, null);
+        return count;
+    }
+
+    @Override
+    public int update(@NonNull Uri uri, @Nullable ContentValues values, @Nullable String selection, @Nullable String[] selectionArgs) {
+        SQLiteDatabase db = mDatabaseHelper.getWritableDatabase();
+        Long now = Long.valueOf(System.currentTimeMillis());
+
+        String tableName;
+        int count;
+
+        switch (mUriMatcher.match(uri)) {
+            case TEST_PLUGIN:
+                tableName = PluginTable.TABLE_NAME;
+                if (!values.containsKey(PluginTable.MODIFIED_DATE)) {
+                    values.put(PluginTable.MODIFIED_DATE, now);
+                }
+                break;
+            case TEST_PLUGIN_ID:
+                tableName = PluginTable.TABLE_NAME;
+                if (!values.containsKey(PluginTable.MODIFIED_DATE)) {
+                    values.put(PluginTable.MODIFIED_DATE, now);
+                }
+                selection = PluginTable._ID + "=" + uri.getPathSegments().get(1)
+                        + (!TextUtils.isEmpty(selection) ? " AND (" + selection + ')' : "");
+                break;
+            default:
+                throw new IllegalArgumentException("Unknown URI " + uri);
+        }
+
+        count = db.update(tableName, values, selection, selectionArgs);
+        getContext().getContentResolver().notifyChange(uri, null);
+        return count;
+    }
+
+    @Nullable
+    @Override
+    public Cursor query(@NonNull Uri uri, @Nullable String[] projection, @Nullable String selection, @Nullable String[] selectionArgs, @Nullable String sortOrder) {
+        Log.d("TAG_HOOK", "PluginContentProvider -> query : uri = " + uri + ", selection = " + selection + ", projection = " + projection + ", selectionArgs = " + selectionArgs + ",sortOrder = " + sortOrder);
+        String tableName;
+        switch (mUriMatcher.match(uri)) {
+            case TEST_PLUGIN:
+                tableName = PluginTable.TABLE_NAME;
+                if (projection == null || projection.length == 0) {
+                    projection = sTestPluginProjection;
+                }
+                if (TextUtils.isEmpty(sortOrder)) {
+                    sortOrder = PluginTable.SORT_ORDER;
+                }
+                break;
+            case TEST_PLUGIN_ID:
+                tableName = PluginTable.TABLE_NAME;
+                if (projection == null || projection.length == 0) {
+                    projection = sTestPluginProjection;
+                }
+                if (TextUtils.isEmpty(sortOrder)) {
+                    sortOrder = PluginTable.SORT_ORDER;
+                }
+                if (TextUtils.isEmpty(selection)) {
+                    selection = PluginTable._ID + " = " + uri.getPathSegments().get(1);
+                } else {
+                    selection += " AND " + PluginTable._ID + " = " + uri.getPathSegments().get(1);
+                }
+                break;
+            default:
+                throw new IllegalArgumentException("Unknown URI " + uri);
+        }
+
+        SQLiteDatabase db = mDatabaseHelper.getReadableDatabase();
+        Cursor cursor = db.query(tableName, projection, selection, selectionArgs, null, null, sortOrder);
+        return cursor;
+    }
+
+
+    public static final class PluginTable implements BaseColumns {
+        //表名
+        public static final String TABLE_NAME = "plugin_table";
+        //表的属性
+        public static final String PLUGIN_NAME = "plugin_name";
+        public static final String CREATE_DATE = "create_date";
+        public static final String MODIFIED_DATE = "modified_date";
+        public static final String SORT_ORDER = "modified_date DESC";
+    }
+
+    private static class DatabaseHelper extends SQLiteOpenHelper {
+        //数据库名和版本
+        public static final String DATABASE_NAME = "plugin_database.db";
+        public static final int DATABASE_VERSION = 2;
+
+        public DatabaseHelper(Context context) {
+            super(context, DATABASE_NAME, null, DATABASE_VERSION);
+        }
+
+        @Override
+        public void onCreate(SQLiteDatabase db) {
+            db.execSQL("CREATE TABLE IF NOT EXISTS " + PluginTable.TABLE_NAME + "("
+                    + PluginTable._ID + " INTEGER PRIMARY KEY, "
+                    + PluginTable.PLUGIN_NAME + " TEXT, "
+                    + PluginTable.CREATE_DATE + " INTEGER, "
+                    + PluginTable.MODIFIED_DATE + " INTEGER );");
+        }
+
+        @Override
+        public void onOpen(SQLiteDatabase db) {
+            db.execSQL("CREATE TABLE IF NOT EXISTS " + PluginTable.TABLE_NAME + "("
+                    + PluginTable._ID + " INTEGER PRIMARY KEY, "
+                    + PluginTable.PLUGIN_NAME + " TEXT, "
+                    + PluginTable.CREATE_DATE + " INTEGER, "
+                    + PluginTable.MODIFIED_DATE + " INTEGER );");
+        }
+
+        @Override
+        public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+            //trick，直接删除重建
+            db.execSQL("DROP TABLE IF EXISTS " + PluginTable.TABLE_NAME);
+            onCreate(db);
+        }
+    }
+}
