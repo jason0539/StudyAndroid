9fdf480496772048274bc4bee8831b5652781e9e
jason0539 <jason0539@163.com>
2017-09-28 09:51:54 +0800

    update:线程池注释

--- common/src/main/java/com/jason/common/utils/ThreadUtil.java
+++ common/src/main/java/com/jason/common/utils/ThreadUtil.java
@@ -3,22 +3,72 @@ package com.jason.common.utils;
 import android.os.Handler;
 import android.os.Looper;
 
+import java.util.concurrent.BlockingQueue;
 import java.util.concurrent.Callable;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Future;
 import java.util.concurrent.LinkedBlockingQueue;
+import java.util.concurrent.ThreadFactory;
 import java.util.concurrent.ThreadPoolExecutor;
 import java.util.concurrent.TimeUnit;
+import java.util.concurrent.atomic.AtomicInteger;
 
 /**
  * Created by liuzhenhui on 2016/10/29.
  */
 public class ThreadUtil {
-    private final static int NUM_OF_CORES = Runtime.getRuntime().availableProcessors();
+    private final static int CPU_COUNT = Runtime.getRuntime().availableProcessors();
+    /**
+     * 核心线程数
+     */
+    private static final int CORE_POOL_SIZE = CPU_COUNT;
+    /**
+     * 最大线程数量
+     */
+    private static final int MAXMUM_POOL_SIZE = CPU_COUNT * 2 + 1;
+    /**
+     * 非核心线程闲置时间为1秒
+     */
+    private static final int KEEP_TIME = 1;
+
+    /**
+     * 线程池任务队列
+     */
+    private static final BlockingQueue<Runnable> mPoolWorkQueue = new LinkedBlockingQueue<>();
+
+    /**
+     * 线程工厂类
+     */
+    private static final ThreadFactory mThreadFactory = new ThreadFactory() {
+
+        private final AtomicInteger mCount = new AtomicInteger(1);
+
+        @Override
+        public Thread newThread(Runnable r) {
+            return new Thread(r, "Thread Current ID = " + mCount.getAndIncrement());
+        }
+    };
 
     //非UI线程池
-    private static ExecutorService executorService =
-            new ThreadPoolExecutor(2, 2, 0L, TimeUnit.MILLISECONDS, new LinkedBlockingQueue<Runnable>());
+    /*
+    if(currThreadNum < CoreThreadNum){
+        new Thread();
+        currThreadNum++;
+    }else{
+        if(workQueue.isFull()){
+            if(currThreadNum < MaxThreadNum){
+                new Thread();
+                currThreadNum++;
+            }else{
+                throw Exception();
+            }
+        }else{
+            workQueue.addTask();
+        }
+    }
+     */
+    private static ExecutorService executorService = new ThreadPoolExecutor(CORE_POOL_SIZE, MAXMUM_POOL_SIZE, KEEP_TIME, TimeUnit.SECONDS, mPoolWorkQueue, mThreadFactory);
+
     //UI线程
     private static Handler mUiHandler = new Handler(Looper.getMainLooper());
 
@@ -29,7 +79,7 @@ public class ThreadUtil {
         executorService.execute(new Runnable() {
             @Override
             public void run() {
-                MLog.d(MLog.TAG_JSON,"UtilThread->run 线程：" + Thread.currentThread().getName());
+                MLog.d(MLog.TAG_THREAD, "UtilThread->run 线程：" + Thread.currentThread().getName());
                 runnable.run();
             }
         });
