8ad934994d4222a9851d730c0448a65a59ad8085
jason0539 <jason0539@163.com>
2017-12-19 11:35:47 +0800

    update:recycle view viewType用法demo

--- app/src/main/java/com/jason/workdemo/demo/recycleview/NormalRecyclerViewAdapter.java
+++ app/src/main/java/com/jason/workdemo/demo/recycleview/NormalRecyclerViewAdapter.java
@@ -1,6 +1,7 @@
 package com.jason.workdemo.demo.recycleview;
 
 import android.content.Context;
+import android.graphics.Color;
 import android.support.v7.widget.RecyclerView;
 import android.util.Log;
 import android.view.LayoutInflater;
@@ -16,19 +17,30 @@ import com.jason.workdemo.R;
 
 public class NormalRecyclerViewAdapter extends RecyclerView.Adapter<NormalRecyclerViewAdapter.NormalTextViewHolder> {
 
+    public static final String VIEW_TITLE_ANDROID = "Android";
+    public static final String VIEW_TITLE_IOS = "iOS";
+
+    public static final int VIEW_TYPE_ANDROID = 1;
+    public static final int VIEW_TYPE_IOS = 2;
+
     private final LayoutInflater mLayoutInflater;
-    private final Context mContext;
     private String[] mTitles;
 
     public NormalRecyclerViewAdapter(Context context) {
-        mTitles = new String[]{"iOS", "Android"};
-        mContext = context;
+        mTitles = new String[]{VIEW_TITLE_ANDROID, VIEW_TITLE_IOS};
         mLayoutInflater = LayoutInflater.from(context);
     }
 
     @Override
     public NormalTextViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
-        return new NormalTextViewHolder(mLayoutInflater.inflate(R.layout.item_main_list, parent, false));
+        View view = mLayoutInflater.inflate(R.layout.item_main_list, parent, false);
+        TextView textView = (TextView) view.findViewById(R.id.tv_item_main_list);
+        if (viewType == VIEW_TYPE_ANDROID) {
+            textView.setTextColor(Color.GREEN);
+        } else if (viewType == VIEW_TYPE_IOS) {
+            textView.setTextColor(Color.RED);
+        }
+        return new NormalTextViewHolder(view);
     }
 
     @Override
@@ -39,6 +51,19 @@ public class NormalRecyclerViewAdapter extends RecyclerView.Adapter<NormalRecycl
     @Override
     public int getItemCount() {
         return mTitles == null ? 0 : mTitles.length;
+    }
+
+    @Override
+    public int getItemViewType(int position) {
+        if (mTitles.length > position) {
+            String title = mTitles[position];
+            if (title.equals(VIEW_TITLE_ANDROID)) {
+                return VIEW_TYPE_ANDROID;
+            } else if (title.equals(VIEW_TITLE_IOS)) {
+                return VIEW_TYPE_IOS;
+            }
+        }
+        return super.getItemViewType(position);
     }
 
     public static class NormalTextViewHolder extends RecyclerView.ViewHolder {
