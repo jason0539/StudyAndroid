780dc9af30a7857f7c0c85c080151a4f1c55d229
jason0539 <jason0539@163.com>
2018-05-08 17:36:48 +0800

    免继承扩展类功能

---
+++ app/src/main/java/com/jason/workdemo/kotlin/ext.kt
@@ -0,0 +1,8 @@
+package com.jason.workdemo.kotlin
+
+import android.content.Context
+import android.widget.Toast
+
+fun Context.toast(msg: String, duration: Int = Toast.LENGTH_SHORT) {
+    Toast.makeText(this, msg, Toast.LENGTH_SHORT).show()
+}
