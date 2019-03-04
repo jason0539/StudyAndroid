8d4a6dfbf0fab1a8666c2bd3d011b8ee83a6ca1f
jason0539 <jason0539@163.com>
2019-03-04 16:34:00 +0800

    插件重命名

--- gradle-plugin/src/main/groovy/com/jason/demo/gradle/test/TimePlugin.groovy
+++ gradle-plugin/src/main/groovy/com/jason/demo/gradle/test/TimePlugin.groovy
@@ -3,7 +3,7 @@ package com.jason.demo.gradle.test
 import org.gradle.api.Plugin
 import org.gradle.api.Project
 
-public class PluginImpl implements Plugin<Project> {
+public class TimePlugin implements Plugin<Project> {
     void apply(Project project) {
         project.gradle.addListener(new TimeListener())
         project.task('testTask') << {
