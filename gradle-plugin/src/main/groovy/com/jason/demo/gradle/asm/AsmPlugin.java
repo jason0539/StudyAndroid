8b4876e1d4c1c9f54d54eed20cce67a9751cd223
jason0539 <jason0539@163.com>
2019-03-05 17:50:12 +0800

    增加transform用法Demo

---
+++ gradle-plugin/src/main/groovy/com/jason/demo/gradle/asm/AsmPlugin.java
@@ -0,0 +1,22 @@
+package com.jason.demo.gradle.asm;
+
+import com.android.build.gradle.AppExtension;
+
+import org.gradle.api.Plugin;
+import org.gradle.api.Project;
+import org.gradle.api.logging.Logger;
+import org.gradle.api.logging.Logging;
+
+/**
+ * Created by daoming.lzh on 2019/3/4
+ */
+public class AsmPlugin implements Plugin<Project> {
+    static final Logger logger = Logging.getLogger(AsmPlugin.class);
+
+    @Override
+    public void apply(Project project) {
+        logger.lifecycle("asm============================AsmPlugin.apply");
+        AppExtension android = project.getExtensions().getByType(AppExtension.class);
+        android.registerTransform(new AsmPluginTransform(project));
+    }
+}
